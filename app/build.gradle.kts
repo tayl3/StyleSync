59beb9d8187baa44d9c7cd6c4b617cd635062408
Taylor <tjedwards2@wisc.edu>
2024-03-18 18:11:21 -0500

    Modified gradle files to accommodate firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -107,7 +108,10 @@ dependencies {
     implementation("androidx.navigation:navigation-ui:2.6.0")
     implementation("com.google.code.gson:gson:2.10")
     implementation("org.postgresql:postgresql:42.2.5")
-
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+    implementation("androidx.annotation:annotation:1.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
