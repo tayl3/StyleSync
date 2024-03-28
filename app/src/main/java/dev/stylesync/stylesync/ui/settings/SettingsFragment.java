d5c99a9862d53ab186bb70b8c95f5b9958603422
tayl3 <120345291+tayl3@users.noreply.github.com>
2024-03-27 22:42:50 -0500

    Fixed sign in, added icons for sign in/out, added navigation to home after signin, implemented signout, added welcome messages for guest and user

--- app/src/main/java/dev/stylesync/stylesync/ui/settings/SettingsFragment.java
+++ app/src/main/java/dev/stylesync/stylesync/ui/settings/SettingsFragment.java
@@ -12,16 +12,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -29,6 +35,7 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.Arrays;
 import java.util.List;
 
+import dev.stylesync.stylesync.R;
 import dev.stylesync.stylesync.databinding.FragmentSettingsBinding;
 
 
@@ -36,6 +43,8 @@ public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
     private SettingsViewModel settingsViewModel;
+    private ImageButton signInButton;
+    private ImageButton signOutButton;
 
     // See: https://developer.android.com/training/basics/intents/result
     private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
@@ -64,15 +73,51 @@ public class SettingsFragment extends Fragment {
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.GoogleBuilder().build());
 
-        // Create and launch sign-in intent
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build();
+        signInButton = root.findViewById(R.id.google_sign_in_button);
+        signOutButton = root.findViewById(R.id.sign_out_button);
 
-        signInLauncher.launch(signInIntent);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null) {
+            signInButton.setVisibility(View.GONE);
+            signOutButton.setVisibility(View.VISIBLE);
+            setWelcomeText(user.getDisplayName());
+        } else {
+            signInButton.setVisibility(View.VISIBLE);
+            signOutButton.setVisibility(View.GONE);
+            setWelcomeText(null);
+        }
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create and launch sign-in intent
+                Intent signInIntent = AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build();
+                signInLauncher.launch(signInIntent);
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                signInButton.setVisibility(View.VISIBLE);
+                signOutButton.setVisibility(View.GONE);
+            }
+        });
 
         return root;
+    }
+
+    private void setWelcomeText(String username) {
+        final TextView welcomeTextView = binding.welcomeTextView;
+        if(username != null) {
+            settingsViewModel.setUsername(username);
+            welcomeTextView.setText("Welcome, " + username + "!");
+        } else {
+            welcomeTextView.setText("Welcome, GUEST!");
+        }
     }
 
     @Override
@@ -87,18 +132,52 @@ public class SettingsFragment extends Fragment {
             // Successfully signed in
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             settingsViewModel.setAuthenticated(true);
+            if(user != null && user.getDisplayName() != null) {
+                settingsViewModel.setUsername(user.getDisplayName());
+                settingsViewModel.setUserId(user.getUid());
+            } else {
+                settingsViewModel.setUsername("GUEST");
+                settingsViewModel.setUserId("NULL");
+            }
+
+            final TextView welcomeTextView = binding.welcomeTextView;
+            welcomeTextView.setText("Welcome, " + settingsViewModel.getUsername() + "!");
+
+            //Navigate to home screen
+            NavController navController = NavHostFragment.findNavController(this);
+            navController.navigate(R.id.action_settings_to_home);
         } else {
             // Sign in failed. If response is null the user canceled the
             // sign-in flow using the back button. Otherwise check
             // response.getError().getErrorCode() and handle the error.
             // ...
             if(response == null) {
-
+                Log.d("SettingsFragment", "User cancelled login");
             } else {
                 int errorCode = response.getError().getErrorCode();
+                Log.d("SettingsFragment", "Error code: " + errorCode);
             }
 
+            settingsViewModel.setUsername("GUEST");
+            settingsViewModel.setUserId("NULL");
             settingsViewModel.setAuthenticated(false);
         }
+    }
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(requireContext())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<Void> task) {
+                        // ...
+                        settingsViewModel.setUsername("GUEST");
+                        settingsViewModel.setUserId("NULL");
+                        settingsViewModel.setAuthenticated(false);
+                        if(binding != null) {
+                            final TextView welcomeTextView = binding.welcomeTextView;
+                            welcomeTextView.setText("Welcome, GUEST!");
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/dev/stylesync/stylesync/ui/settings/SettingsViewModel.java
+++ app/src/main/java/dev/stylesync/stylesync/ui/settings/SettingsViewModel.java
@@ -1,20 +1,24 @@
 package dev.stylesync.stylesync.ui.settings;
 
+import android.mtp.MtpConstants;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class SettingsViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
     private final MutableLiveData<String> mText;
     private final MutableLiveData<Boolean> mIsAuthenticated; // Indicates if the user is authenticated
-
+    private final MutableLiveData<String> mUsername;
+    private final MutableLiveData<String> mUserId;
 
     public SettingsViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is settings fragment");
         mIsAuthenticated = new MutableLiveData<>();
         mIsAuthenticated.setValue(false);
+        mUsername = new MutableLiveData<>();
+        mUserId = new MutableLiveData<>();
     }
 
     public LiveData<String> getText() {
@@ -25,8 +29,22 @@ public class SettingsViewModel extends ViewModel {
         return mIsAuthenticated;
     }
 
+    public LiveData<String> getUsername() {
+        return mUsername;
+    }
+    public LiveData<String> getUserId() {
+        return mUserId;
+    }
+
     public void setAuthenticated(boolean isAuthenticated) {
         mIsAuthenticated.setValue(isAuthenticated);
     }
 
+    public void setUsername(String username) {
+        mUsername.setValue(username);
+    }
+
+    public void setUserId(String userId) {
+        mUserId.setValue(userId);
+    }
 }
